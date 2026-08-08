48c83271e75c6958eed3fb6b17bc47749698ab3c
Charannsai <pathurisai31@gmail.com>
2026-08-08 21:31:56 +0530

    feat: add reusable SVG icon component library to UI directory

--- src/components/ui/hugeicons.tsx
+++ src/components/ui/hugeicons.tsx
@@ -325,3 +325,10 @@ export const CheckIcon: React.FC<IconProps> = ({ size = 20, className = '', ...p
   </svg>
 );
 
+export const CopyIcon: React.FC<IconProps> = ({ size = 20, className = '', ...props }) => (
+  <svg width={size} height={size} {...defaultProps} className={className} {...props}>
+    <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
+    <path d="M5 15H4a2 2 0 01-2-2V4a2 2 0 012-2h9a2 2 0 012 2v1" />
+  </svg>
+);
+
